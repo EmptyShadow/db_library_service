bf1460668ffe5b7f0b26b4833f77fce7d6d27dad
EmptyShadow <dimon6407@gmail.com>
2018-03-04 18:21:12 +0300

    add connection to database

--- config/connections.js
+++ config/connections.js
@@ -42,7 +42,7 @@ module.exports.connections = {
   ***************************************************************************/
   someMysqlServer: {
     adapter: 'sails-mysql',
-    host: 'http:\\mysql/',
+    host: 'http://db:3306/',
     user: 'root', //optional
     password: '123', //optional
     database: 'library' //optional
