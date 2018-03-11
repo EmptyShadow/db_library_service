97c088dc4cf834d46c174e3d771953a63f68d0f3
EmptyShadow <dimon6407@gmail.com>
2018-03-11 16:52:16 +0300

    Implement login and registration(join) in UserController, but no tested

--- api/controllers/AuthorController.js
+++ api/controllers/AuthorController.js
@@ -7,26 +7,7 @@
 
 module.exports = {
 	login: function (req, res) {
-
-        let data = {
-            login: req.params('login'),
-            email: req.params('email'),
-            password: md5(req.params('password'))
-        };
-
-        User.findOne(data).exec(function (err, user) {
-            if (err) {
-                return res.serverError(err);
-            }
-
-            if (!user) {
-                return res.notFound('Неверный логин, почта или пароль!!');
-            }
-
-            // создание сессии и кукис
-
-            return res.json(user);
-        })
+        
     }
 };
 
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,28 +6,98 @@
  */
 
 module.exports = {
+
+    /**
+     * Авторизация 
+     */
     login: function (req, res) {
+        // получаем данные из запроса
         let data = {
             login: req.param('login'),
             email: req.param('email'),
             password: req.param('password')
         }
+        // если что то пусто
         if (!data.login || !data.email || !data.password) {
+            // то говорим что запрос неверный
             return res.badRequest('Все поля должны быть заполнены!!');
         }
-        return res.view('homepage');
+
+        // по переданным данным ищем пользователя
+        User.findOne(data).exec(function (err, user) {
+            if (err) {
+                return res.serverError(err);
+            }
+
+            if (!user) {
+                return res.notFound('Неверный логин, почта или пароль!!');
+            }
+
+            // создание сессии и кукис
+
+            // Возвращаем данные пользователя
+            user.password = '';
+            return res.json(user);
+        })
     },
 
+    /**
+     * Регистрация
+     */
     join: function (req, res) {
+        // получаем данные из запроса
         let data = {
             login: req.param('login'),
             email: req.param('email'),
             password1: req.param('password1'),
             password2: req.param('password2')
         }
+
+        // если что то пусто
         if (!data.login || !data.email || !data.password1 || !data.password2) {
+            // то говорим что запрос неверный
             return res.badRequest('Все поля должны быть заполнены!!');
         }
-        return res.view('homepage');
+        // если подтверждение пароля не верное
+        if (data.password1 != data.password2) {
+            // то сообщаем об этом
+            return res.badRequest('Пароли не совпадают!!');
+        }
+
+        // пытаемся найти пользователя у которого логин или почта совпадают с переданными
+        User.quary(
+            'SELECT * FROM user WHERE user.login = $1 OR user.email = $2 LIMIT 0,1', 
+            [data.login, data.email], 
+            function (err, user) {
+                if (err) {
+                    return res.serverError(err);
+                }
+                // если пользователь нашелся
+                if (user) {
+                    // формируем ответ, что совпало
+                    let errRes = {
+                        errLogin: data.login === user.login ? 'Логин занят!!' : '',
+                        errEmail: data.email === user.email ? 'Почта занята!!' : ''
+                    };
+                    return res.badRequest(errRes);
+                }
+                
+                // создаем нового пользователя
+                User.create(data).exec(function (err, newUser) {
+                    if (err) {
+                        return res.serverError(err);
+                    }
+                    if (!newUser) {
+                        if (err) {
+                            return res.serverError({err: 'Неудалось создать пользователя!!', data});
+                        }
+                    }
+
+                    // возвращаем данные нового пользователя
+                    newUser.password = '';
+                    return res.json(newUser);
+                });
+            }
+        );
     }
 };
