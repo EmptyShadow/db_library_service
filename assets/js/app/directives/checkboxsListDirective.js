d7384ab5732f00814a5745dcccb9bb4bf9517692
EmptyShadow <dimon6407@gmail.com>
2018-04-17 22:05:40 +0300

    implement directive for show list checkboxes and touch processing

---
+++ assets/js/app/directives/checkboxsListDirective.js
@@ -0,0 +1,84 @@
+(function () {
+    'use strict';
+
+    angular.module('Library').directive('listCheckboxs', listCheckboxs);
+
+    listCheckboxs.$inject = [];
+    function listCheckboxs() {
+        return {
+            replace: true,
+            restrict: 'E',
+            scope: { checkboxes: '=' },
+            template: '<div class="form-inline mb-3">'
+                + '<div class="form-check ml-2">'
+                + '<div class="custom-control custom-checkbox">'
+                + '<input type="checkbox" class="custom-control-input" id="master" ng-model="master" ng-change="masterChange()">'
+                + '<label class="custom-control-label" for="master" ng-if="master">Снять со всех выбор</label>'
+                + '<label class="custom-control-label" for="master" ng-if="!master">Выбрать все</label>'
+                + '</div>'
+                + '</div>'
+                + '<div ng-repeat="cb in checkboxes">'
+                + '<div class="form-check ml-2">'
+                + '<div class="custom-control custom-checkbox">'
+                + '<input type="checkbox" class="custom-control-input" id="checkbox{{$index}}" ng-model="cb.isSelected" ng-change="cbChange()" ng-click="click($event, cb)">'
+                + '<label class="custom-control-label" for="checkbox{{$index}}">{{ cb.desc }}</label>'
+                + '</div>'
+                + '</div>'
+                + '</div>'
+                + '</div>',
+            controller: function ($scope, $element) {
+                $scope.master = false;
+                $scope.masterChange = function () {
+                    let elements = angular.element('input.custom-control-input');
+                    console.log(elements);
+                    if ($scope.master) {
+                        angular.forEach($scope.checkboxes, function (cb, index) {
+                            if (cb.isSelected != undefined) {
+                                cb.isSelected = true;
+                            }
+                        });
+                    } else {
+                        angular.forEach($scope.checkboxes, function (cb, index) {
+                            if (cb.isSelected != undefined) {
+                                cb.isSelected = false;
+                            }
+                        });
+                    }
+                };
+
+                let masterCb = $element.children()[0].getElementsByTagName("input")[0];
+                $scope.cbChange = function () {
+                    let allSet = true, allClear = true;
+                    angular.forEach($scope.checkboxes, function (cb, index) {
+                        if (cb.isSelected) {
+                            allClear = false;
+                        } else if (cb.isSelected != undefined) {
+                            allSet = false;
+                        }
+                    });
+                    if (allSet && !allClear) {
+                        $scope.master = true;
+                        masterCb.indeterminate = false;
+                        return;
+                    }
+                    else if (allClear && !allSet) {
+                        $scope.master = false;
+                        masterCb.indeterminate = false;
+                        return;
+                    }
+                    else {
+                        $scope.master = false;
+                        masterCb.indeterminate = true;
+                    }
+                };
+                $scope.cbChange();  // initialize
+                $scope.click = function (event, cb) {
+                    if (event.ctrlKey) {
+                        cb.isSelected = undefined;
+                        event.currentTarget.indeterminate = true;
+                    }
+                }
+            },
+        };
+    }
+})();
